beebc620ab7b26b9123de07c5b6bae461ad5c873
axelguerrero <guerreroaxel230@yahoo.com>
2022-12-08 15:28:26 -0500

    fixed navbar, changed ui on contact form

--- views/contact.jsx
+++ views/contact.jsx
@@ -1,86 +1,31 @@
 import React, { useState } from 'react';
 import Def from './default';
-import { PlusOutlined } from '@ant-design/icons';
-import {
-  Form,
-  Input,
-  Select,
-  Button,
-  Upload,
-} from 'antd';
-
-const { TextArea } = Input;
+import {Box, Button, TextField, MenuItem} from "@mui/material"
 
 function Contact(data) {
   return(
     <Def>
       <main>
         <body>
-          <h1 className='logo'>Contact us</h1>
-          <Form
-            labelCol={{
-            span: 4,
-          }}
-          wrapperCol={{
-           span: 10,
-         }}
-          layout="horizontal"
-        >
-          <Form.Item 
-            label="Name"
-            name="Name"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your name!',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item 
-            label="Email"
-            name="Email"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your email!',
-              },
-            ]}>
-            <Input />
-          </Form.Item>
-
-          <Form.Item 
-            label="Issue"
-            name="Issue"
-            rules={[
-              {
-                required: true,
-                message: 'Please select your issue!',
-              },
-            ]}>
-            <Select placeholder="Please select one">
-              <Select.Option value="My Notes aren't Appearing">My Notes aren't Appearing</Select.Option>
-              <Select.Option value="The website is glitchy">The website is glitchy</Select.Option>
-              <Select.Option value="I can't make a new set of notes">I can't make a new set of notes</Select.Option>
-              <Select.Option value="I cant invite people to my notes">I cant invite people to my notes</Select.Option>
-              <Select.Option value="Other">Other</Select.Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item label="Description">
-            <TextArea rows={4} />
-          </Form.Item>
-
-          <Form.Item>
-            <Button>Submit</Button>
-              </Form.Item>
-            </Form>
-          </body>
-        </main>
-      </Def>
-    )
-  }
+          <h1>Contact us</h1>
+          <div className="contactForm">
+            <form>
+              <div>
+              <TextField id="outlined-basic" label="Name" variant="outlined" />
+              </div>
+              <div>
+              <TextField id="outlined-basic" label="Email" variant="outlined" />
+              </div>
+              <div>
+                <Button variant="outlined">Submit</Button>
+              </div>
+            </form>
+              </div>
+            </body>
+          </main>
+        </Def>
+      )
+    }
  
 
 export default Contact;
