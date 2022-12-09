95b54e8db5676a93aa6a9c5a22584a947ed41616
axelguerrero <guerreroaxel230@yahoo.com>
2022-12-09 13:08:58 -0500

    finished contact page/styling

--- views/contact.jsx
+++ views/contact.jsx
@@ -1,6 +1,15 @@
 import React, { useState } from 'react';
 import Def from './default';
-import {Box, Button, TextField, MenuItem} from "@mui/material"
+import { PlusOutlined } from '@ant-design/icons';
+import { 
+  Form,
+  Input, 
+  Button, 
+} from 'antd';
+import { color } from '@mui/system';
+
+
+const { TextArea } = Input;
 
 function Contact(data) {
   return(
@@ -9,17 +18,59 @@ function Contact(data) {
         <body>
           <h1>Contact us</h1>
           <div className="contactForm">
-            <form>
-              <div>
-              <TextField id="outlined-basic" label="Name" variant="outlined" />
-              </div>
-              <div>
-              <TextField id="outlined-basic" label="Email" variant="outlined" />
-              </div>
-              <div>
-                <Button variant="outlined">Submit</Button>
-              </div>
-            </form>
+          <Form 
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 8 }}
+            layout="horizontal"
+          >
+            <Form.Item 
+              label={<label style={{ color: "white" }}>Name</label>}
+              name="name"
+              rules={[
+            {
+              required: true,
+              message: "Please input your username!"
+            }
+          ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item 
+              label={<label style={{ color: "white" }}>Email</label>}
+              name="email"
+              rules={[
+            {
+              required: true,
+              message: "Please input your email!"
+            }
+          ]}
+              >
+              <Input />
+            </Form.Item>
+            <Form.Item 
+            label={<label style={{ color: "white" }}>Issue</label>}
+            name="issue"
+             rules={[
+           {
+             required: true,
+             message: "Please input your issue!"
+           }
+         ]}
+             >
+              <Input />
+            </Form.Item>
+            <Form.Item 
+              label={<label style={{ color: "white" }}>Description</label>}
+              name="description"
+              >
+              <TextArea rows={4} />
+            </Form.Item>
+            <Form.Item
+            align="center"
+            >
+              <Button type="primary">Submit</Button>
+            </Form.Item>
+          </Form>
               </div>
             </body>
           </main>
