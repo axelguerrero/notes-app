f2e1fbc20a16fc876b1bdb4f71bff9547d53e2b8
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-12-10 10:39:05 -0500

    finished about page

--- views/about.jsx
+++ views/about.jsx
@@ -1,22 +1,20 @@
-import React from 'react'
-import Def from './default'
+import React from "react";
+import Def from "./default";
 
 function About() {
-    return (
-        <Def>
-            <h1>About</h1>
-            <h3>Milestone Project
-                Created By:</h3>
-            <h4>
-                Axel Guerrero, 
-                Lucian Mbenza, 
-                Shane Spillman
-            </h4>
-            <div>
-            <img src="https://cdn.quotesgram.com/img/37/62/69911863-sticky-notes-funny-facebook-cover-t2.jpg" alt="sticky note goes here"></img>
-            </div>
-        </Def>
-    )
+  return (
+    <Def>
+      <h1>About</h1>
+      <h3>Milestone Project Created By:</h3>
+      <h4>Axel Guerrero, Lucian Nzuzi Mbenza, Shane Spillman</h4>
+      <div>
+        <img
+          src="https://cdn.quotesgram.com/img/37/62/69911863-sticky-notes-funny-facebook-cover-t2.jpg"
+          alt="sticky note goes here"
+        ></img>
+      </div>
+    </Def>
+  );
 }
 
 export default About;
