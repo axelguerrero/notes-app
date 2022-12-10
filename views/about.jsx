78ae9e138f5a60bd0300445748ba1f48bcc4cd54
samsdogjack <105887707+samsdogjack@users.noreply.github.com>
2022-12-10 09:29:44 -0500

    additions to about page

--- views/about.jsx
+++ views/about.jsx
@@ -5,7 +5,16 @@ function About() {
     return (
         <Def>
             <h1>About</h1>
-            
+            <h3>Milestone Project
+                Created By:</h3>
+            <h4>
+                Axel Guerrero, 
+                Lucian Mbenza, 
+                Shane Spillman
+            </h4>
+            <div>
+            <img src="https://cdn.quotesgram.com/img/37/62/69911863-sticky-notes-funny-facebook-cover-t2.jpg" alt="sticky note goes here"></img>
+            </div>
         </Def>
     )
 }
